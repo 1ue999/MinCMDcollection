0eb0540a61a3b644ef2d6997b681eb4df1b72fd9
1ue999 <106450442+1ue999@users.noreply.github.com>
2024-01-05 14:34:21 +0100

    Made the code work

--- collection.js
+++ collection.js
@@ -3,11 +3,11 @@
 
 //Unlock everything:
 c=Vars.content;
-c.items().each(e=>e.unlock);
-c.blocks().each(e=>e.unlock);
-c.units().each(e=>e.unlock);
-c.statusEffects().each(e=>e.unlock);
-c.fluids().each(e=>e.unlock);
+c.items().each(e=>e.unlock());
+c.blocks().each(e=>e.unlock());
+c.units().each(e=>e.unlock());
+c.statusEffects().each(e=>e.unlock());
+c.fluids().each(e=>e.unlock());
 /*NOTE: You can use this code to unlock specific things:
 Blocks.router.unlock();
 */
